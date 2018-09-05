60b6aa11c0e78b8592c73f97e3d860abd6502267
Krittiya Clark <krittiyaclark@gmail.com>
2018-09-04 23:26:38 -0400

    Add ImageInput

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
-    state = {
-        contacts: []
-    }
-    componentDidMount() {
-        ContactsAPI.getAll().then((contacts) => {
-            this.setState({ contacts })
-        })
-    }
-    removeContact = (contact) => {
-        this.setState((state) => ({
-            contacts: state.contacts.filter((c) => c.id !== contact.id)
-        }))
+  state = {
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
 
-        ContactsAPI.remove(contact)
-    }
-    render() {
-        return (
-            <div>
-                <ListContacts
-                    onDeleteContact={this.removeContact}
-                    contacts={this.state.contacts}
-                />
-                {this.state.screen === 'list' && (
-                    <ListContacts
-                        onDeleteContact={this.removeContact}
-                        contacts={this.state.contacts}
-                    />
-                )}
-                {this.state.screen === 'create' && (
-                    <CreateContact />
-                )}
-            </div>
-        )
-    }
+    ContactsAPI.remove(contact)
+  }
+  render() {
+    return (
+      <div>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )}/>
+        <Route path='/create' component={CreateContact}/>
+      </div>
+    )
+  }
+}
+
+export default App;
